a78c9e9e6031249857d9313f745ecadb5bc9989c
Molly Dolan <dolanmollye@gmail.com>
2020-11-04 19:18:14 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a && names.start_with? ("A")
+    if suite == :suite_a && names.start_with? "A"
       winner = name
     end
   end
